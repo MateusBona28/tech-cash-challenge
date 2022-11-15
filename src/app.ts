7ac99c476d7516fdd0858faa004a706c38ca4c96
unknown <mateusbona28@gmail.com>
2022-11-15 10:57:47 -0300

    feat: adicionando caminho para rota de login

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import "express-async-errors"
 import express from "express"
 import usersRoutes from "./routes/users.routes"
 import { handleErrorMiddleware } from "./middlewares/errors.middlewares"
+import sessionRoutes from "./routes/session.routes"
 
 
 const app = express()
@@ -10,6 +11,7 @@ app.use(express.json())
 
 
 app.use("/users", usersRoutes)
+app.use("/login", sessionRoutes)
 
 app.use(handleErrorMiddleware)
 
